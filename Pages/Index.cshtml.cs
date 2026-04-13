5b5f37202529d3f6060cf7fac573b2fa9a298673
Brzana <szymonbrzana@gmail.com>
2026-04-13 13:23:34 +0200

    Initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace pitWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
